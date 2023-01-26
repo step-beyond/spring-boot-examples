bedaee9809ea8f24c4f8803d641b9e9a4996fc09
Jesko Steinberg <61695677+jeskosda@users.noreply.github.com>
2023-01-26 14:53:36 +0100

    feat(app): push generated project

---
+++ src/main/java/today/stepbeyond/examples/springbootexamples/SpringBootExamplesApplication.java
@@ -0,0 +1,13 @@
+package today.stepbeyond.examples.springbootexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExamplesApplication.class, args);
+	}
+
+}
