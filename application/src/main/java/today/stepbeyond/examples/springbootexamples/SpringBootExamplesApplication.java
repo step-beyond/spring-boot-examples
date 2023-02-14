e4ce209d1264da74b28c84dd8f24aab4b49b3ff0
Jonas Huebner <ap8588@lan.huk-coburg.de>
2023-02-14 11:30:15 +0100

    refactor: move `@EnableFeignClients` annotation to where feign is defined.

--- application/src/main/java/today/stepbeyond/examples/springbootexamples/SpringBootExamplesApplication.java
+++ application/src/main/java/today/stepbeyond/examples/springbootexamples/SpringBootExamplesApplication.java
@@ -2,10 +2,8 @@ package today.stepbeyond.examples.springbootexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class SpringBootExamplesApplication {
 
   public static void main(String[] args) {
---
+++ infrastructure-petstore-rest-client/src/main/java/today/stepbeyond/examples/springbootexamples/infrastructure/gateways/PetStoreRestClientConfiguration.java
@@ -0,0 +1,8 @@
+package today.stepbeyond.examples.springbootexamples.infrastructure.gateways;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class PetStoreRestClientConfiguration {}
