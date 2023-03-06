302c7dc5c2d0616f386f55c11a8e69259c4ca95a
Jonas Huebner <dev@jonashuebner.de>
2023-03-06 13:13:42 +0100

    refactor(feign-client): introduce default properties for feign client

---
+++ infrastructure-petstore-rest-client/src/main/java/today/stepbeyond/examples/springbootexamples/infrastructure/gateways/PetStoreRestConfiguration.java
@@ -0,0 +1,8 @@
+package today.stepbeyond.examples.springbootexamples.infrastructure.gateways;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:petstore-client-defaults.properties")
+public class PetStoreRestConfiguration { }
